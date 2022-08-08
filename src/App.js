3beab1920144566f08dc2f9562ee25538d0989f4
Miguel C <migs.castaneda@gmail.com>
2022-08-08 14:58:22 -0400

    router in progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { useEffect, useState } from 'react'
-import Gallery from './components/Gallery'
-import SearchBar from './components/SearchBar'
+import { useEffect, useState } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Gallery from "./components/Gallery"
+import SearchBar from "./components/SearchBar"
+import AlbumView from "./components/AlbumView"
+import ArtistView from "./components/ArtistView"
 
 
 const App = () => {
-  let [search, setSearch] = useState('')
-  let [message, setMessage] = useState('Search for Music!')
+  let [search, setSearch] = useState("")
+  let [message, setMessage] = useState("Search for Music!")
   let [data, setData] = useState([])
 
-  const API_URL = 'https://itunes.apple.com/search?term='
+  const API_URL = "https://itunes.apple.com/search?term="
 
   useEffect(() => {
       if(search) {
@@ -19,7 +22,7 @@ const App = () => {
               if (resData.results.length > 0) {
                   return setData(resData.results)
               } else {
-                  return setMessage('Not Found.')
+                  return setMessage("Not Found.")
               }
           }
           fetchData()
@@ -33,9 +36,20 @@ const App = () => {
 
   return (
       <div>
-          <SearchBar handleSearch={handleSearch} />
-          {message}
-          <Gallery data={data} />
+         {message}
+         <Router>
+            <Routes>
+                <Route path="/" element={
+                    <Fragment>
+                        <SearchBar handleSearch={handleSearch} /> 
+                        <Gallery data={data} />
+                    </Fragment>
+                } />
+                <Route path="/album/:id" element={<AlbumView />} />
+                <Route path="/artist/:id" element={<ArtistView />} />
+            </Routes>
+         </Router>
+
       </div>
   )
 }
